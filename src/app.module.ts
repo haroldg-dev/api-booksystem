5cc914fe52e60baaf037914de57c05f22575c334
Cihanyarali <cihan.yaralii@gmail.com>
2024-10-30 00:49:04 -0700

    Added Booking module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { PersonModule } from './modules/person/person.module';
+import { BookingModule } from './booking/booking.module';
 
 @Module({
-  imports: [PersonModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    PersonModule,
+    BookingModule,
+  ],
   controllers: [],
   providers: [],
 })
---
+++ src/booking/booking.controller.ts
@@ -0,0 +1,32 @@
+import { Controller, Get, Post, Put, Delete, Param, Body } from '@nestjs/common';
+import { BookingService } from './booking.service';
+
+@Controller('booking')
+export class BookingController {
+    constructor(private readonly bookingService: BookingService) {}
+
+    @Get()
+    async getAllBookings() {
+        return await this.bookingService.getAllBookings();
+    }
+
+    @Get(':id')
+    async getBookingById(@Param('id') id: string) {
+        return await this.bookingService.getBookingById(id);
+    }
+
+    @Post()
+    async createBooking(@Body() bookingData) {
+        return await this.bookingService.createBooking(bookingData);
+    }
+
+    @Put(':id')
+    async updateBooking(@Param('id') id: string, @Body() updateData) {
+        return await this.bookingService.updateBooking(id, updateData);
+    }
+
+    @Delete(':id')
+    async deleteBooking(@Param('id') id: string) {
+        return await this.bookingService.deleteBooking(id);
+    }
+}
