3d320d80be4de0309a6f33b5a9081ed6a9ee7ff3
Harold Gutierrez <haroooldg@gmail.com>
2024-11-24 12:59:50 -0800

    fix person

--- src/modules/person/dto/create-person.dto.ts
+++ src/modules/person/dto/create-person.dto.ts
@@ -3,7 +3,7 @@ import { IsEmail, IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 export class CreatePersonDto {
   @IsString()
-  @IsNotEmpty()
+  @IsOptional()
   id: string;
 
   @IsString()
